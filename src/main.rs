88b9e38c1ca3873e0485a279e7861511744cdee4
Kay Gosho <ketsume0211@gmail.com>
2021-12-08 18:48:14 +0900

    add sway subscription part

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,45 @@
 use evdev::{Device, InputEventKind, Key};
 use std::error::Error;
+use std::sync::{Arc, Mutex};
 use std::{thread, time};
+use swayipc::{reply, Connection, EventType};
 use uinput;
 use uinput_sys::EV_KEY;
-
-fn get_fallback_device() -> Result<Device, Box<dyn Error>> {
-    for i in 0..25 {
-        let device = Device::open(format!("/dev/input/event{}", i))?;
-        if device
-            .supported_keys()
-            .map_or(false, |keys| keys.contains(Key::KEY_ENTER))
-        {
-            return Ok(device);
-        }
-    }
-    panic!("Cannot infer default device");
-}
 
 const CAPS: u16 = 58;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut device = get_fallback_device()?;
+    let remap_enabled = Arc::new(Mutex::new(false));
+    let mut handles = vec![];
+
+    // TODO: get sway process from root permission (or vice versa)
+    let conn = Connection::new()?;
+
+    /////////////////////////
+    // Sway subscribe part
+    /////////////////////////
+    let remap_enabled_cloned = Arc::clone(&remap_enabled);
+    handles.push(thread::spawn(move || {
+        let mut stream = conn
+            .subscribe(&[EventType::Window])
+            .expect("Unable to subscribe sway");
+        loop {
+            let window_class = get_window_class(stream.next());
+            let mut remap_enabled_lock = remap_enabled_cloned.lock().unwrap();
+
+            if window_class == "Brave-browser" {
+                println!("brave found!");
+                *remap_enabled_lock = true;
+            } else {
+                *remap_enabled_lock = false;
+            }
+        }
+    }));
+
+    /////////////////////////
+    // Keyboard part
+    /////////////////////////
+    let mut device = get_keyboard_device()?;
 
     let mut virtual_input = uinput::default()?
         .name("/dev/uinput")?
@@ -36,8 +55,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut is_caps_pressing = false;
 
-    loop {
-        let events = device.fetch_events()?;
+    let remap_enabled_cloned_2 = Arc::clone(&remap_enabled);
+    handles.push(thread::spawn(move || loop {
+        let remap_enabled_lock = *remap_enabled_cloned_2.lock().unwrap();
+        if remap_enabled_lock == false {
+            continue;
+        }
+        let events = device.fetch_events().unwrap();
         events.for_each(|event| {
             println!("{:?}", event);
             match event.kind() {
@@ -61,11 +85,44 @@ fn main() -> Result<(), Box<dyn Error>> {
                 _ => {}
             }
         });
+    }));
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 
     // TODO: add cleanup function
-    //
     // device.ungrab()?;
     //
-    // Ok(())
+    Ok(())
+}
+
+fn get_keyboard_device() -> Result<Device, Box<dyn Error>> {
+    for i in 0..25 {
+        let device = Device::open(format!("/dev/input/event{}", i))?;
+        if device
+            .supported_keys()
+            .map_or(false, |keys| keys.contains(Key::KEY_ENTER))
+        {
+            return Ok(device);
+        }
+    }
+    panic!("Cannot infer default device");
+}
+
+fn get_window_class(evt: Option<Result<reply::Event, swayipc::Error>>) -> String {
+    match evt {
+        Some(Ok(window_event)) => match window_event {
+            reply::Event::Window(b) => match (b.container.app_id, b.container.window_properties) {
+                (Some(id), _) => id,
+                (_, Some(props)) => match props.class {
+                    Some(class) => class,
+                    _ => panic!("Cannot get window id"),
+                },
+                _ => panic!("Cannot get window id"),
+            },
+            _ => panic!("Cannot get window id"),
+        },
+        _ => panic!("Cannot get window id"),
+    }
 }
