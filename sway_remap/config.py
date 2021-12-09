4e1ebb7105c08bd00b9f8826514fea647a2d85c3
Kay Gosho <ketsume0211@gmail.com>
2021-12-09 23:13:24 +0900

    meta key support

--- sway_remap/config.py
+++ sway_remap/config.py
@@ -20,13 +20,16 @@ class Binding:
             return evdev.ecodes.KEY_B  # type:ignore
         raise Exception("Not Found")
 
-    def map_if_ctrl(self) -> bool:
+    def only_ctrl(self) -> bool:
         return 'ctrl' in self.remap
+
+    def only_alt(self) -> bool:
+        return 'alt' in self.remap
 
 
 example_config = [
     Binding('ctrl.f', [uinput.KEY_RIGHT]),
     Binding('ctrl.b', [uinput.KEY_LEFT]),
-    # Binding('alt.f', [uinput.KEY_LEFTCTRL, uinput.KEY_RIGHT]),
-    # Binding('alt.b', [uinput.KEY_LEFTCTRL, uinput.KEY_LEFT]),
+    Binding('alt.f', [uinput.KEY_LEFTCTRL, uinput.KEY_RIGHT]),
+    Binding('alt.b', [uinput.KEY_LEFTCTRL, uinput.KEY_LEFT]),
 ]
--- sway_remap/main.py
+++ sway_remap/main.py
@@ -7,6 +7,10 @@ from all_keys import ALL_KEYS
 from config import example_config, Binding
 
 CTRL_KEYS = [evdev.ecodes.KEY_CAPSLOCK]  # type: ignore
+ALT_KEYS = [
+    evdev.ecodes.KEY_LEFTALT,  # type: ignore
+    evdev.ecodes.KEY_RIGHTALT,  # type: ignore
+]
 
 EV_KEY = evdev.ecodes.EV_KEY  # type: ignore
 
@@ -20,6 +24,7 @@ def run(config: list[Binding], path: str):
     print(real_input)
 
     is_ctrl = False
+    is_alt = False
 
     with uinput.Device(ALL_KEYS) as virtual_uinput:
         time.sleep(1)  # Important delay
@@ -28,23 +33,31 @@ def run(config: list[Binding], path: str):
                 if event.type == EV_KEY:
                     if event.code in CTRL_KEYS:
                         is_ctrl = is_pressed(event.value)
+                    if event.code in ALT_KEYS:
+                        is_alt = is_pressed(event.value)
                     handled = False
                     for binding in config:
-                        if (is_ctrl
+                        pass_ctrl = is_ctrl and binding.only_ctrl()
+                        pass_alt = is_alt and binding.only_alt()
+                        if ((pass_ctrl or pass_alt)
                                 and event.code == binding.get_remap_keycode()
                                 and is_pressed(event.value)):
                             handled = True
                             virtual_uinput.emit(uinput.KEY_CAPSLOCK, 0)
+                            virtual_uinput.emit(uinput.KEY_LEFTALT, 0)
                             virtual_uinput.emit_combo(binding.to)
-                            virtual_uinput.emit(uinput.KEY_CAPSLOCK, 1)
+                            if is_ctrl:
+                                virtual_uinput.emit(uinput.KEY_CAPSLOCK, 1)
+                            if is_alt:
+                                virtual_uinput.emit(uinput.KEY_LEFTALT, 1)
                     if not handled:
                         virtual_uinput.emit((0x01, event.code), event.value)
 
+
+if __name__ == '__main__':
+    run(example_config, '/dev/input/event4')
 
 # TODO: list-device helper
 # devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 # for device in devices:
 #    print(device.path, device.name, device.phys)
-
-if __name__ == '__main__':
-    run(example_config, '/dev/input/event4')
