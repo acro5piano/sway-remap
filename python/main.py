442bb8ae1d545e5c01ecdc31c1449bbdda70b9e4
Kay Gosho <ketsume0211@gmail.com>
2021-12-09 21:43:47 +0900

    refactor

--- python/main.py
+++ python/main.py
@@ -9,7 +9,7 @@ import evdev
 
 KEYS = [
     uinput.KEY_LEFTALT, uinput.KEY_TAB, uinput.KEY_E, uinput.KEY_O,
-    uinput.KEY_RIGHT, uinput.KEY_CAPSLOCK
+    uinput.KEY_LEFT, uinput.KEY_RIGHT, uinput.KEY_CAPSLOCK
 ]
 
 CTRL_KEYS = [evdev.ecodes.KEY_CAPSLOCK]  # type: ignore
@@ -28,3 +28,5 @@ with uinput.Device(KEYS) as virtual_uinput:
                     is_ctrl = event.value == 1 or event.value == 2
                 if is_ctrl and event.code == evdev.ecodes.KEY_F and event.value == 1:  # type: ignore
                     virtual_uinput.emit_combo([uinput.KEY_RIGHT])
+                if is_ctrl and event.code == evdev.ecodes.KEY_B and event.value == 1:  # type: ignore
+                    virtual_uinput.emit_combo([uinput.KEY_LEFT])
